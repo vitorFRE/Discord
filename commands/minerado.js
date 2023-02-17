95e3b987dde945a2e5d503388a4725b1a1180fee
Vitor <frete497@gmail.com>
2023-02-16 20:07:50 -0400

    Base

---
+++ commands/minerado.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('minerado')
+    .setDescription('responda com foi minerado'),
+  async execute(interaction) {
+    await interaction.reply('Você foi Minerado');
+  },
+};
